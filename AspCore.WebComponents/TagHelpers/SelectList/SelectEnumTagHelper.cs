0c795a78bb9fd270eb79c99260d42420caeaf7c7
Yusuf Aykaç <yusuf_aykac@hotmail.com>
2020-02-24 15:49:10 +0300

    define a selectlist from enums with description

---
+++ AspCore.WebComponents/TagHelpers/SelectList/SelectEnumTagHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AspCore.WebComponents.TagHelpers.SelectList
+{
+    public class SelectEnumTagHelper:TagHelper
+    {
+
+        public int SelectedValue { get; set; }
+        public Type EnumType { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "select";
+            foreach (int e in Enum.GetValues(EnumType))
+            {
+                var op = new TagBuilder("option");
+                op.Attributes.Add("value", $"{e}");
+                var displayText = GetEnumFieldDisplayName(e);
+                op.InnerHtml.Append(displayText);
+                if (e == SelectedValue)
+                    op.Attributes.Add("selected", "selected");
+                output.Content.AppendHtml(op);
+            }
+        }
+        private string GetEnumFieldDisplayName(int value)
+        {
+            var fieldName = Enum.GetName(EnumType, value);
+            var displayName = EnumType.GetField(fieldName).GetCustomAttributes(false).OfType<DisplayAttribute>().SingleOrDefault()?.Name;
+            return displayName ?? fieldName;
+        }
+    }
+}
