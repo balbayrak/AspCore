0b6efee6bf070d08aee8a5852b1c76dfe3bf0995
balbayrak <balbayrak87@gmail.com>
2020-04-21 02:05:13 +0300

    redis client

---
+++ AspCore.RedisClient/Configuration/RedisCacheOption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspCore.RedisClient.Configuration
+{
+    public class RedisCacheOption
+    {
+        public string instanceName { get; set; }
+        public string[] servers { get; set; }
+        /// <summary>
+        /// use for redis sentinels
+        /// </summary>
+        public string masterName { get; set; }
+    }
+}
---
+++ AspCore.RedisClient/Configuration/RedisCacheOptionBuilder.cs
@@ -0,0 +1,40 @@
+﻿using AspCore.Caching.Abstract;
+using AspCore.Caching.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspCore.RedisClient.Configuration
+{
+    public static class RedisCacheOptionBuilder
+    {
+        public static void AddRedisCache(this CacheOptionBuilder cacheOptionBuilder, Action<RedisCacheOption> option)
+        {
+            RedisCacheOption redisCacheOption = new RedisCacheOption();
+            option(redisCacheOption);
+
+            if (redisCacheOption.servers.Length > 1)
+            {
+                var sentinel = new RedisSentinel(redisCacheOption.servers, redisCacheOption.masterName);
+                sentinel.RedisManagerFactory = (master, slaves) => new RedisManagerPool(master);
+                cacheOptionBuilder.services.AddSingleton<IRedisClientsManager>(c => sentinel.Start());
+            }
+            else
+            {
+                cacheOptionBuilder.services.AddSingleton<IRedisClientsManager>(c =>
+                 new PooledRedisClientManager(redisCacheOption.servers)
+                 {
+                     ConnectTimeout = 100,
+                 });
+            }
+
+            cacheOptionBuilder.services.AddSingleton(typeof(ICacheService), sp =>
+            {
+                IRedisClientsManager manager = sp.GetService<IRedisClientsManager>();
+                return new RedisCacheManager(manager);
+            });
+        }
+    }
+}
