e1436659dfef823a52b17b95428547cf1c8a59c9
Yusuf Aykaç <yusuf_aykac@hotmail.com>
2021-02-21 16:27:48 +0300

    fx middleware.

--- AspCore.WebApi/Middlewares/ExceptionMiddleware.cs
+++ AspCore.WebApi/Middlewares/ExceptionMiddleware.cs
@@ -4,19 +4,25 @@ using Microsoft.AspNetCore.Http;
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace AspCore.WebApi.Middlewares
 {
     public class ExceptionMiddleware
     {
         private readonly RequestDelegate _next;
-        //private readonly ILoggerManager _logger;
-
-        // public ExceptionMiddleware(RequestDelegate next, ILoggerManager logger)
+        private  readonly JsonSerializerSettings _jsonSettings;
         public ExceptionMiddleware(RequestDelegate next)
         {
-            //_logger = logger;
             _next = next;
+            _jsonSettings = new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                Formatting = Formatting.Indented,
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore
+            };
         }
 
         public async Task InvokeAsync(HttpContext httpContext)
@@ -27,23 +33,23 @@ namespace AspCore.WebApi.Middlewares
             }
             catch (Exception ex)
             {
-                //_logger.LogError($"Something went wrong: {ex}");
+              
                 await HandleExceptionAsync(httpContext, ex);
             }
         }
 
-        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        private  Task HandleExceptionAsync(HttpContext context, Exception exception)
         {
             context.Response.ContentType = "application/json";
             context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-
-            return context.Response.WriteAsync(new BaseServiceResult()
+            var data = new BaseServiceResult()
             {
                 StatusCode = context.Response.StatusCode,
                 ErrorMessage = WebApiConstants.BaseExceptionMessages.INTERNAL_SERVER_ERROR_OCCURRED,
                 ExceptionMessage = exception.Message + "---> stacktrace:" + exception.StackTrace,
-
-            }.ToString());
+            };
+            var jsonContent = JsonConvert.SerializeObject(data, _jsonSettings);
+            return context.Response.WriteAsync(jsonContent);
         }
     }
 }
