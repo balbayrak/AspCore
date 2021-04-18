90c7b6554610818e8da26043afcfb99a46d537ce
balbayrak87@gmail.com <balbayrak87@gmail.com>
2021-04-19 01:15:36 +0300

    bug fix

--- AspCore.Utilities/Encryption/HashingHelper.cs
+++ AspCore.Utilities/Encryption/HashingHelper.cs
@@ -1,28 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace AspCore.Utilities
 {
     public static class HashingHelper
     {
-        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        public static void CreateHash(string text, out byte[] hash, out byte[] salt)
         {
             using (var hmac = new System.Security.Cryptography.HMACSHA512())
             {
-                passwordSalt = hmac.Key;
-                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                salt = hmac.Key;
+                hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(text));
             }
         }
 
-        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        public static string CreateHash(string text, string key)
         {
-            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            using (MemoryStream stream = new MemoryStream(encoding.GetBytes(key)))
             {
-                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                using (var hmac = new System.Security.Cryptography.HMACSHA512(stream.ToArray()))
+                {
+                    Byte[] bytes = hmac.ComputeHash(encoding.GetBytes(text));
+
+                    return System.Convert.ToBase64String(bytes);
+                }
+            }
+        }
+
+
+
+        public static bool VerifyHash(string text, byte[] hash, byte[] salt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(salt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(text));
                 for (int i = 0; i < computedHash.Length; i++)
                 {
-                    if (computedHash[i] != passwordHash[i])
+                    if (computedHash[i] != hash[i])
                     {
                         return false;
                     }
